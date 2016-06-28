c5ad2bbe7a142fb13a91ab0950325c5b015b2a76
josep <josefermoso@gmail.com>
2016-06-28 14:30:11 -0300

    Added mongo Lab

--- app.js
+++ app.js
@@ -34,7 +34,22 @@ var paginate = require('express-paginate');
 app.use(paginate.middleware(10, 50));
 app.use('/posts', posts);
 
-mongoose.connect("mongodb://localhost:27017/test");
+
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    process.env.MONGODB_URI ||
+    'mongodb://localhost/HelloMongoose';
+
+//mongoose.connect("mongodb://localhost:27017/test");
+
+mongoose.connect(uristring, function(err, res) {
+    if (err) {
+        console.log('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + uristring);
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
